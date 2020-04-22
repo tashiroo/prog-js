fec99d3bd0dc89d596c7ffd1fc8cfdb1f586d56c
taashiroo <ing631tom@outlook.jp>
2020-04-22 23:28:00 +0900

    4月22日の課題

---
+++ 8 hensuunohenkou2/script.js
@@ -0,0 +1,12 @@
+let number = 7;
+console.log(number);
+
+// 変数numberの値に3を加えてください
+number = number+3;
+// number += 3; でも可
+console.log(number);
+
+// 変数numberの値を2で割ってください
+number = number/2
+// number /= 2; でも可
+console.log(number);
